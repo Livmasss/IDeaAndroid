95a9fb77c663f54cd6049d2e29cae35052e538a9
livmas <seiranyan.sg@gmail.com>
2022-10-15 21:59:01 +0300

    Empty activity_register2.xml created

--- app/src/main/java/com/livmas/idea/RegisterActivity.kt
+++ app/src/main/java/com/livmas/idea/RegisterActivity.kt
@@ -2,10 +2,14 @@ package com.livmas.idea
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.EditText
 
 class RegisterActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_register)
+
+        val nameEditText : EditText = findViewById(R.id.userName)
+        val passwordEditText : EditText = findViewById(R.id.userEmail)
     }
 }
