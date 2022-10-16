383df949c2fa97d8acfc253f6d22650252eb6614
livmas <seiranyan.sg@gmail.com>
2022-10-17 00:22:27 +0300

    RegisterActivity1.kt and RegisterActivity2.kt without avatar picker

--- app/src/main/java/com/livmas/idea/RegisterActivity1.kt
+++ app/src/main/java/com/livmas/idea/RegisterActivity1.kt
@@ -3,8 +3,10 @@ package com.livmas.idea
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.TextUtils
 import android.widget.Button
 import android.widget.EditText
+import android.widget.Toast
 
 class RegisterActivity1 : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -14,15 +16,39 @@ class RegisterActivity1 : AppCompatActivity() {
         val emailEditText : EditText = findViewById(R.id.userEmail)
         val nameEditText : EditText = findViewById(R.id.userName)
         val nextButton : Button = findViewById(R.id.nextButton)
+        val avatarSelectButton : Button = findViewById(R.id.avatarSelectButton)
 
         nextButton.setOnClickListener() {
+
             val name = nameEditText.text.toString()
             val email = emailEditText.text.toString()
-            val intent = Intent(this, RegisterActivity2::class.java)
 
-            intent.putExtra("name", name)
-            intent.putExtra("email", email)
-            startActivity(intent)
+            if ( isEmailValid(email) && isNameValid(name)  ) {
+                val intent1 = Intent(this, RegisterActivity2::class.java)
+
+                intent1.putExtra("name", name)
+                intent1.putExtra("email", email)
+                startActivity(intent1)
+            }
         }
+    }
+
+    private fun isNameValid(name : String) : Boolean {
+        if ( name.length < 3 ) {
+            Toast.makeText(this, "Длина имени меньше 3 символов", Toast.LENGTH_SHORT).show()
+            return false
+        }
+        return true
+    }
+    fun isEmailValid(email : String): Boolean {
+        if ( TextUtils.isEmpty(email) ) {
+            Toast.makeText(this, "Email пуст", Toast.LENGTH_SHORT).show()
+            return false
+        }
+        if ( !android.util.Patterns.EMAIL_ADDRESS.matcher(email).matches() ) {
+            Toast.makeText(this, "Неверный email адрес", Toast.LENGTH_SHORT).show()
+            return false
+        }
+        return true
     }
 }
--- app/src/main/java/com/livmas/idea/RegisterActivity2.kt
+++ app/src/main/java/com/livmas/idea/RegisterActivity2.kt
@@ -1,18 +1,56 @@
 package com.livmas.idea
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 
 class RegisterActivity2 : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_register2)
 
-        val name = intent.getStringExtra("name")
+        val passwordView : EditText = findViewById(R.id.userPassword)
+        val passConfirmView : EditText = findViewById(R.id.userPasswordConfimation)
+        val nextButton : Button = findViewById(R.id.nextButton)
+        var password : String
 
-        if (name != null) {
-            Log.d("reg", name)
+        nextButton.setOnClickListener {
+            password = passwordView.text.toString()
+
+            if ( isPasswordValid(password, passConfirmView.text.toString() ) ) {
+                val intent2 = Intent(this, RegisterActivity3::class.java)
+                intent2.apply {
+                    putExtra("name", intent.getStringExtra("name"))
+                    putExtra("email", intent.getStringExtra("email"))
+                    putExtra("password", password)
+                }
+                startActivity(intent2)
+            }
         }
+    }
+
+    fun isPasswordValid (password : String, passConfirm : String) : Boolean {
+        val passLen = password.length
+        if ( passLen < 8 ) {
+            Toast.makeText(this, "Пароль должен быть не менее 8 символов", Toast.LENGTH_SHORT).show()
+            return false
+        }
+
+        if ( password != passConfirm ) {
+            Toast.makeText(this, "Пароли не совпадают!", Toast.LENGTH_SHORT).show()
+            return false
+        }
+
+        for ( i in password ) {
+            if ( i.isDigit() ) {
+                return true
+            }
+        }
+        Toast.makeText(this, "Пароль должен содержать цифру", Toast.LENGTH_SHORT).show()
+        return false
     }
 }
