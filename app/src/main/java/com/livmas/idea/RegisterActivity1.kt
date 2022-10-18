b604b55d87a6199fbcb15a02c3fe5433ffae86e3
livmas <seiranyan.sg@gmail.com>
2022-10-18 16:44:40 +0300

    Avatar picker

--- app/src/main/java/com/livmas/idea/RegisterActivity1.kt
+++ app/src/main/java/com/livmas/idea/RegisterActivity1.kt
@@ -1,14 +1,27 @@
 package com.livmas.idea
 
+import android.app.Activity
+import android.content.Context
 import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.text.TextUtils
 import android.widget.Button
 import android.widget.EditText
+import android.widget.ImageView
 import android.widget.Toast
+import androidx.activity.result.PickVisualMediaRequest
+import androidx.activity.result.contract.ActivityResultContract
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.net.toUri
 
 class RegisterActivity1 : AppCompatActivity() {
+    lateinit var avatarView : ImageView
+    private val imagePickerLauncher = registerForActivityResult(ActivityResultContracts.GetContent()) {
+        uri: Uri? -> uri.let { avatarView.setImageURI(uri) }
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_register1)
@@ -17,6 +30,11 @@ class RegisterActivity1 : AppCompatActivity() {
         val nameEditText : EditText = findViewById(R.id.userName)
         val nextButton : Button = findViewById(R.id.nextButton)
         val avatarSelectButton : Button = findViewById(R.id.avatarSelectButton)
+        avatarView = findViewById(R.id.avatar)
+
+        avatarSelectButton.setOnClickListener {
+            uploadImage(avatarView)
+        }
 
         nextButton.setOnClickListener() {
 
@@ -33,6 +51,11 @@ class RegisterActivity1 : AppCompatActivity() {
         }
     }
 
+    private fun uploadImage(avatarView: ImageView) {
+
+        imagePickerLauncher.launch("image/*")
+    }
+
     private fun isNameValid(name : String) : Boolean {
         if ( name.length < 3 ) {
             Toast.makeText(this, "Длина имени меньше 3 символов", Toast.LENGTH_SHORT).show()
@@ -40,7 +63,7 @@ class RegisterActivity1 : AppCompatActivity() {
         }
         return true
     }
-    fun isEmailValid(email : String): Boolean {
+    private fun isEmailValid(email : String): Boolean {
         if ( TextUtils.isEmpty(email) ) {
             Toast.makeText(this, "Email пуст", Toast.LENGTH_SHORT).show()
             return false
